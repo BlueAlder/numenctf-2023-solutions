8d30ea9b0e217331155995e2548de792daf2ce07
Sam Calamos <sam.calamos@gmail.com>
2023-04-10 23:38:06 +1000

    bruh

--- script/ASSLOT/Asslot.sol
+++ script/ASSLOT/Asslot.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
-import "../src/asslot/asslot.sol";
-import "../src/asslot/solve.sol";
 
 contract AsslotScript is Script {
     address contractAddress = vm.envAddress("CONTRACT_ADDRESS");
