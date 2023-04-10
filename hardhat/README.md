2af3e652afcad5fabae4ca16179b99a5d8bcbdf2
Sam Calamos <sam.calamos@gmail.com>
2023-04-10 20:41:09 +1000

    add hexp solution

---
+++ hardhat/README.md
@@ -0,0 +1,8 @@
+# Hardhat Solutions
+
+This hardhat project contains the solutions for the challenge that I could
+not complete in Foundry for whatever reason.
+
+The list of challenges include:
+
+- Hexp
--- script/hexp.sol
+++ script/hexp.sol
@@ -1,22 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "forge-std/Script.sol";
-import "forge-std/console.sol";
-import "../src/hexp/hexp.sol";
+// Blockhash(Block number - 0xa) & 0xffffff
+// Gasprice & 0xffffff
 
-contract WalletScript is Script {
-    address contractAddress = vm.envAddress("CONTRACT_ADDRESS");
-    uint256 playerKey = vm.envUint("PRIVATE_KEY");
-    Hexp challenge;
+// This challenge requires setting the gasPrice of the transaction to be
+// the same value as the last 3 bytes of the blockhash from 10 blocks prior
+// since you can't easily dynamically set the gasPrice from within foundry
+// the solve script for this is located in the hardhat directory of this repo
 
-    function setUp() public {
-        challenge = new Hexp();
-    }
-
-    function run() public {
-        // Blockhash(Block number - 0xa) & 0xffffff
-        // Gasprice & 0xffffff
-        require(challenge.isSolved(), "challenge not solved");
-    }
-}
+// hardhat/scripts/hexp.ts
